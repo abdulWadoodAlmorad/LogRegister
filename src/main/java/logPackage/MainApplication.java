a6d8f5f040d462cf7f5e6004963634874c516534
abdulWadoodAlmorad <abdoalmorad1995@gmail.com>
2024-07-05 09:20:53 +0200

    comments with English

--- src/main/java/logPackage/MainApplication.java
+++ src/main/java/logPackage/MainApplication.java
@@ -6,11 +6,7 @@ public class MainApplication {
 
          LogReader.main(args);
 
-        // Execute LogProcessor to estimate log similarity and modify them
-        // Führen Sie LogProcessor aus, um die Ähnlichkeit der Protokolle zu schätzen und diese zu ändern
-        LogProcessor.main(args);
-
-        // Rufen Sie die Funktion processLogContent aus der Klasse LogTester auf
+   
         LogTester.processLogContent("");
     }
 }
